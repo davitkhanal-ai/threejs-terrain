f64ff95160ffb58595da60dc474071511add6d38
davitkhanal <khanaldavit@gmail.com>
2021-03-24 22:36:14 +0545

    displacement and alpha map added

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ import './style.css'
 import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+const loader = new THREE.TextureLoader()
+const height = loader.load("height.png")
+const texture = loader.load('./texture.jpg')
+const alpha = loader.load('./alpha.png')
 
 // Debug
 const gui = new dat.GUI()
@@ -18,7 +22,13 @@ const geometry = new THREE.PlaneBufferGeometry(3 , 3, 64, 64)
 
 // Materials
 const material = new THREE.MeshStandardMaterial({
-    color:'gray'
+    color:'gray',
+    map: texture,
+    displacementMap: height,
+    displmentScale : .6,
+    alphaMap:alpha,
+    transparent:true,
+    depthTest :false
 })
 
 const plane = new THREE.Mesh(geometry, material)
@@ -32,10 +42,10 @@ gui.add(plane.rotation, 'x').min(0).max(600)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 2)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight('#00b3ff', 2)
+pointLight.position.x = .2
+pointLight.position.y = 10
+pointLight.position.z = 4.4
 scene.add(pointLight)
 
 gui.add(pointLight.position, 'x')
@@ -97,6 +107,12 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+document.addEventListener('mousemove',animateTerrain)
+let mouseY = 0
+function animateTerrain(event){
+    mouseY = event.clientY
+}
+
 const clock = new THREE.Clock()
 
 const tick = () =>
@@ -106,6 +122,9 @@ const tick = () =>
 
     // Update objects
     // sphere.rotation.y = .5 * elapsedTime
+
+    plane.rotation.z = .5 * elapsedTime
+    plane.material.displacementScale = .3 + mouseY * 0.0009
 
     // Update Orbital Controls
     // controls.update()
